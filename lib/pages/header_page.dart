02968d2ad5542e053a095217302433bfcd2e7499
Alex Espinoza <alexabraham.esp@gmail.com>
2021-04-17 20:35:29 -0400

    square and rounded header

--- lib/pages/header_page.dart
+++ lib/pages/header_page.dart
@@ -1,12 +1,11 @@
+import 'package:design/widgets/headers.dart';
 import 'package:flutter/material.dart';
 
 class HeaderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('hola'),
-      ),
+      body: HeaderRoundedBorders(),
     );
   }
 }
